e2b05002a1172420244f1ee6d34100bf4a9cb917
harry-potter <kritishajain@gmail.com>
2013-07-09 21:52:08 +0530

    Added exception handling

--- learningruby/Connect2DB.rb
+++ learningruby/Connect2DB.rb
@@ -11,12 +11,37 @@ class Connect2DB
                 :port =>'5432'
                 )
         end
+        
+        #Disconnect the backend connection
+        def disconnect
+                @conn.close
+        end
+        
         # Create our test table (assumes it doesn't already exist)
         def createUserTable
-                @conn.exec("CREATE TABLE users (id serial NOT NULL, name character varying(255), CONSTRAINT users_pkey PRIMARY KEY (id)) WITH (OIDS=FALSE);");
+                begin 
+                        connect
+                        @conn.exec("CREATE TABLE users (id serial NOT NULL, name character varying(255), CONSTRAINT users_pkey PRIMARY KEY (id)) WITH (OIDS=FALSE);");
+                rescue Exception => e
+                        puts e.message
+                ensure
+                        disconnect
+                end
+        end
+        
+        #add data to user table
+        def addUser(id,user)
+                begin
+                        connect
+                        @conn.exec("insert INTO users values(#{id},'#{user}')")
+                rescue Exception => e
+                        puts e.message
+                ensure
+                        disconnect
+                end
         end
   end
   ptest = Connect2DB.new()
-  ptest.connect
   ptest.createUserTable
+  ptest.addUser(3,"Test")
   
